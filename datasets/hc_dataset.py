b53d89ca4e24668962ce349da0ab5e62487e7564
Barney Hill <barneyh@NDPH9082.local>
2023-07-08 17:59:42 +0100

    Calculate specificed number initial similarities

--- datasets/hc_dataset.py
+++ datasets/hc_dataset.py
@@ -12,35 +12,39 @@ from datasets.triples import generate_all_triples, samples_triples
 class HCDataset(data.Dataset):
     """Hierarchical clustering dataset."""
 
-    def __init__(self, features, labels, similarities, num_samples):
+    def __init__(self, genotypes, samples, n_triplets):
         """Creates Hierarchical Clustering dataset with triples.
 
         @param labels: ground truth labels
         @type labels: np.array of shape (n_datapoints,)
-        @param similarities: pairwise similarities between datapoints
-        @type similarities: np.array of shape (n_datapoints, n_datapoints)
         """
-        self.features = features
-        self.labels = labels
-        self.similarities = torch.from_numpy(similarities)
-        self.n_nodes = self.similarities.shape[0]
-        self.triples = torch.from_numpy(self.generate_triples(num_samples).astype("int64"))
+        self.genotypes = genotypes
+        self.samples = samples
+        self.n_samples, self.seq_length = genotypes.shape
+        self.triplets, self.similarities = self.create_triplets(n_triplets)
 
     def __len__(self):
-        return len(self.triples)
+        return len(self.triplets)
 
     def __getitem__(self, idx):
-        triple = self.triples[idx]
-        similarities = torch.tensor([self.similarities[triple[0], triple[1]], 
-                                     self.similarities[triple[0], triple[2]], 
-                                     self.similarities[triple[1], triple[2]]])
+        triple = self.triplets[idx]
+        similarities = self.similarities[idx]
+
         return triple, similarities
 
-    def generate_triples(self, num_samples):
-        logging.info("Generating triples.")
-        if num_samples < 0:
-            triples = generate_all_triples(self.n_nodes)
-        else:
-            triples = samples_triples(self.n_nodes, num_samples=num_samples)
-        logging.info(f"Total of {triples.shape[0]} triples")
-        return triples.astype("int64")
+    def create_triplets(self, n_triplets):
+        # Randomly generate triplets
+        triplets = torch.randint(len(self.samples), (n_triplets, 3))
+
+        # Extract all d1, d2 and d3
+        d1 = self.genotypes[triplets[:, 0]]
+        d2 = self.genotypes[triplets[:, 1]]
+        d3 = self.genotypes[triplets[:, 2]]
+
+        # Calculate the similarity for each pair
+        similarities = torch.zeros((n_triplets, 3))
+        similarities[:, 0] = torch.sum((d1 - d2)**2, dim=1) / self.seq_length
+        similarities[:, 1] = torch.sum((d1 - d3)**2, dim=1) / self.seq_length
+        similarities[:, 2] = torch.sum((d2 - d3)**2, dim=1) / self.seq_length
+
+        return triplets, similarities
